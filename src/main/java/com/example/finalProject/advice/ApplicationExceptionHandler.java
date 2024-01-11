59cfddf0098a59a837eb97a780d396a73cc9c4f7
maxsimus.ernest <maxsimus.ernest@gmail.com>
2024-01-11 23:53:40 +0700

    fixing bugs

--- src/main/java/com/example/finalProject/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/example/finalProject/advice/ApplicationExceptionHandler.java
@@ -1,47 +1,40 @@
-package com.example.finalProject;
+package com.example.finalProject.advice;
 
 import com.example.finalProject.dto.ErrorDTO;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
-@ControllerAdvice
-public class RestResponseEntityExceptionHandler
-        extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public final ResponseEntity<Object> handleDBEntityNotFound(EntityNotFoundException ex, WebRequest webRequest) {
-        System.out.println(ex);
         ErrorDTO error = new ErrorDTO();
         error.setCode(HttpStatus.UNPROCESSABLE_ENTITY);
         List<String> message = new ArrayList<>();
         message.add(ex.getMessage());
         error.setMessage(message);
-        return new ResponseEntity<Object>(error, error.getCode());
+        return new ResponseEntity<>(error, error.getCode());
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        System.out.println(ex);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleInvalidArgument(MethodArgumentNotValidException ex) {
         ErrorDTO error = new ErrorDTO();
         error.setCode(HttpStatus.BAD_REQUEST);
-        List<String> message = new ArrayList<String>();
+        List<String> message = new ArrayList<>();
         List<String> collect = ex.getBindingResult().getFieldErrors().stream().filter(Objects::nonNull)
-                .map(m -> (m.getField() + " " + m.getDefaultMessage())).collect(Collectors.toList());
+                .map(m -> (m.getField() + " " + m.getDefaultMessage())).toList();
         message.addAll(collect);
         error.setMessage(message);
-        return new ResponseEntity<Object>(error, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
