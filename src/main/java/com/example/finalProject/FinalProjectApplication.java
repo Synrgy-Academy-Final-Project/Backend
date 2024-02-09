1b20fec2a7c57783cc1ea2f0c24aadc8fe669397
fajartu <twentyone1998@gmail.com>
2024-02-10 02:38:42 +0700

    Update FinalProjectApplication.java

--- src/main/java/com/example/finalProject/FinalProjectApplication.java
+++ src/main/java/com/example/finalProject/FinalProjectApplication.java
@@ -3,11 +3,19 @@ package com.example.finalProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
+
+		Path rootDir = Paths.get("").toAbsolutePath();
+
+		// Print the root directory
+		System.out.println("Root Directory: " + rootDir.toString());
 	}
 
 }
